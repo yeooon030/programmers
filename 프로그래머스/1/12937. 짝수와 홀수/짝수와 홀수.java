7a75a8914179d863035ff3afa1e9e69b656137c6
이수연 <133891777+yeooon030@users.noreply.github.com>
2026-01-11 23:35:02 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -1,9 +1,6 @@
 class Solution {
     public String solution(int num) {
-        if(num == 0 || (num % 2 == 0)) {
-            return "Even";
-        }else{
-            return "Odd";
-        }
+        return num % 2 == 0 ? "Even" : "Odd";
+        
     }
 }
