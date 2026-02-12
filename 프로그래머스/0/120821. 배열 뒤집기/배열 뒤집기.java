850c551a2722af53e1ff09c147b98bc4482d7f06
이수연 <133891777+yeooon030@users.noreply.github.com>
2026-02-12 21:49:33 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,12 +1,10 @@
 class Solution {
     public int[] solution(int[] num_list) {
-        //int[] answer = new int[]
         int length = num_list.length;
-        for(int i=0; i<=length/2; i++){
-            int tmp = num_list[i];
-            num_list[i] = num_list[--length];
-            num_list[length] = tmp;
+        int[] answer = new int[length];
+        for(int i=0; i<length; i++){
+            answer[i] = num_list[length-i-1];
         }
-        return num_list;
+        return answer;
     }
 }
